88cef4c643db806bb3067753d5a6bb65adebd9d1
Mumtaz Haqiqy <mumtaz.haqiqy@bythjul.com>
2023-03-22 08:47:05 +0100

    add upload button

--- pages/Project.py
+++ pages/Project.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.title('Hello in Project')
+
+def read_file(file):
+    file_content = file.read()
+    st.write(file_content)
+
+uploaded_file = st.file_uploader("Choose a file", type="txt")
+
+if uploaded_file is not None:
+    read_file(uploaded_file)
+    
